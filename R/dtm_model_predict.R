e0f518fb857b615436bbab5de63f834f5ab27d33
Swinersha <tomswinfield@gmail.com>
2016-11-18 14:53:15 +0000

    Debugged: Error associated with cbind to produce treepos_dtm, leading to failed gam fitting due to k variable not being correctly interpretted

--- R/dtm_model_predict.R
+++ R/dtm_model_predict.R
@@ -5,6 +5,7 @@
 # Dr Tom Swinfield
 # 17.11.16
 
+#trees=conf.trees_all; model=model; dsm=uav_dsm; chm=uav_chm; predict_grid_by=5; k=100
 
 dtm_model_predict<-function(trees, model, dsm, chm, predict_grid_by, k, type='response')
 {
@@ -17,15 +18,15 @@ dtm_model_predict<-function(trees, model, dsm, chm, predict_grid_by, k, type='re
   
   # Creates the prediction dataframe from the segmented trees:
   segtree<-data.frame(x=trees$maxx, y=trees$maxy, h_uav=trees$CH_max, CR_m=trees$CR_m, sobel=trees$sobel, logsobel=trees$logsobel, normsobel=trees$normsobel, hbound=trees$hbound+trees$sobelbound)
-  segtree<-segtree[complete.cases(segtree_sub),]
+  segtree<-segtree[complete.cases(segtree),]
   
   # Makes the prediction  
   segtree$pred<-predict(model, newdata=segtree)
   
   # Plots out the GAM predictions:
   #par(mfrow=c(1,2), mar=c(4,4,2,2))
-  #plot(x=segtree_sub$h_uav, y=segtree_sub$pred); abline(0,1, col='red')
-  #hist(segtree_sub$h_uav-segtree_sub$pred)
+  #plot(x=segtree$h_uav, y=segtree$pred); abline(0,1, col='red')
+  #hist(segtree$h_uav-segtree$pred)
   
   # Extracts the DSM values for the confidently segmented trees:
   treepos_xy<-data.frame(x=segtree$x, y=segtree$y)
@@ -33,14 +34,16 @@ dtm_model_predict<-function(trees, model, dsm, chm, predict_grid_by, k, type='re
   treepos_dsm<-raster::extract(dsm, treepos)
   treepos_uavchm<-raster::extract(chm, treepos)
   #treepos_lidchm<-raster::extract(lid_chm, treepos)
-  treepos_dtm<-treepos_dsm-segtree_sub$pred
-  treepos_dtm<-cbind(treepos_xy, z=treepos_dtm, w=segtree$logsobel) # You can try playing arond with what you weight by ...
+  treepos_dtm<-treepos_dsm-segtree$pred
+  treepos_dtm<-data.frame(treepos_xy, z=treepos_dtm, w=segtree$logsobel) # You can try playing arond with what you weight by ...
+  Esc.treepos_dtm<<-treepos_dtm
   # I have tried h_uav, hbound and sobel, they are all pretty similar 
   # but sobel looks best
   
   # Estimating the ground 
   cat("\nFitting GAM\n")
-  est.dtm <- mgcv::gam(z ~ s(x, y, k=k), data=treepos_dtm) #, weights=w/mean(w)) # fit the 2D spline.
+  #require(mgcv)
+  est.dtm <-mgcv::gam(z ~ s(x, y, k=k), data=treepos_dtm) #, weights=w/mean(w)) # fit the 2D spline.
   #summary(est.dtm)
   # Create a raster with the predicted values
   grid<-img_grid(dsm, grid_size=predict_grid_by) # produce a grid for prediction.
@@ -53,10 +56,10 @@ dtm_model_predict<-function(trees, model, dsm, chm, predict_grid_by, k, type='re
   
   dtm_pred<-cbind(grid, dtm_pred)
   sp::coordinates(dtm_pred)=~x+y # convert to gridded spatial object
-  sp::proj4string(dtm_pred)<-raster::crs(imagery)
+  sp::proj4string(dtm_pred)<-raster::crs(chm)
   sp::gridded(dtm_pred) <-TRUE
   dtm_pred <- raster::raster(dtm_pred) # convert to raster
-  dtm_pred<-resample(dtm_pred, imagery)
-  dtm_pred<-mask(dtm_pred, imagery)
+  dtm_pred<-resample(dtm_pred, chm)
+  dtm_pred<-mask(dtm_pred, chm)
   return(dtm_pred)
 }
