bea7f0942dc0aaa13c2779399f6a14b1c5d6553e
Swinersha <tomswinfield@gmail.com>
2017-03-01 12:40:10 +0000

    Initial commit

---
+++ R/rq_lut.R
@@ -0,0 +1,42 @@
+#-----------------------------------------------
+# 
+# A function that produces a height to radius lookup table from tau = 0.01 to tau=1
+#
+# Partner functions are included for using the lookup table to convert 
+# from height to radius and vice versa
+#
+# Tom Swinfield
+# 17-02-17
+#
+#-----------------------------------------------
+
+rq_lut<-function(x, y, log){
+  rq_tau<-function(x, y, tau, log){
+    if(log){
+     x<-log(x); y<-log(y) 
+    }
+      
+    fit = rq(y ~ x, tau = tau )
+    a <- coef(fit)[1]
+    b <- coef(fit)[2]
+  
+    params<-data.frame(tau=tau, a=a, b=b)
+    rownames(params)<-NULL
+    return(params)
+  }
+  lut<-lapply((1:100)/100, function(tau) rq_tau(x, y, tau, log))
+  lut<-do.call(rbind, lut)
+  return(lut)
+}
+
+allom_lookup<-function(x, lut, tau, antilog) 
+{
+  rad<-x
+  a<-lut[tau,'a']
+  b<-lut[tau,'b']
+  if(antilog)
+    rad<-(exp(a) * x^b) 
+  else
+    rad<-a + x*b 
+  return(rad)
+}
--- htor_lookup_gta.R
+++ htor_lookup_gta.R
@@ -1,35 +1,16 @@
 require(quantreg)
 require(dplyr)
 
+source("R/htor_lut.R") # The funciton to run the quantile regression
+
 b<-read.csv('/Users/Tom/Dropbox/Tree segmentation/Global tree allometries/GlobalAllometricDatabase/Data-Table 1.csv')
 b<-subset(b, Biogeographic_zone=="Indo-Malaya" & Biome=='Tropical forests' & Functional_type=='Angio')
 
 b$radius<-b$CD/2
 colnames(b)[colnames(b)=="H"]<-"Hmax"
 
-########################### working with 99th percentile#################
-
-#par( mai = c(1.2,1.2,.1,.1))
-#with(b, plot(log(Hmax), log(radius), typ = "n",las = 1, tcl = -.3, xlab = "log(Tree height)", ylab = "log(Crown radius)")) 
-#with(b[b$Soil == "alluvial",], points(log(Hmax), log(radius), pch =16, col ="orange")) 
-#with(b[b$Soil == "kerangas",], points(log(Hmax), log(radius), pch =16, col ="slateblue")) 
-#with(b[b$Soil == "sandstone",], points(log(Hmax), log(radius), pch =16, col ="grey30")) 
-
-
-tau=.99
-htod_lut<-function(tau)
-{
-  ah = rq(log(radius) ~ log(Hmax),  data = b[b$Hmax>20,], tau = tau )
-  aa <- coef(ah)[1]
-  bb <- coef(ah)[2]
-
-  params<-data.frame(tau=tau, aa=aa, bb=bb)
-  rownames(params)<-NULL
-  return(params)
-}
-
-lut<-lapply((1:100)/100, htod_lut)
-lut<-do.call(rbind, lut)
+# Run the quantile regressions:
+lut<-htod_lut(height=b$Hmax, radius=b$radius)
 
 col_ramp<-colorRampPalette(c("blue", "red"))( 100 )
 par(mfrow=c(1,1), mar=c(4,4,2,2))
